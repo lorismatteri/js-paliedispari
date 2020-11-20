278b16c154b8113cae34844795166b3ceeb8122d
Loris Matteri <loris.matteri93@gmail.com>
2020-11-20 17:51:54 +0100

    Parole palindrome
    Inserita funzione per verificare se una parola inserita è palindorma, grazie stackoverflow

--- Palindroma/assets/js/main.js
+++ Palindroma/assets/js/main.js
@@ -1,2 +1,19 @@
 //Chiedere all’utente di inserire una parola
+
+var userWord = prompt("Inserisci una parola");
+
+console.log(userWord);
+
 //Creare una funzione per capire se la parola inserita è palindroma
+
+function palindroma (word) {
+    return word == word.split("").reverse().join("");
+}
+
+console.log(palindroma(userWord));
+
+if (palindroma(userWord) == true) {
+    console.log("La parola è palindorma");
+} else {
+    console.log("La parola non è palindorma");
+}
